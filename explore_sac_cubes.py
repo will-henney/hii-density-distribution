ecc1c45d413b74d092cebcd823c1c72b794441ae
William Henney <whenney@gmail.com>
2026-01-17 22:37:54 -0600

    Calculate 2D PDFs of U-n

--- explore_sac_cubes.py
+++ explore_sac_cubes.py
@@ -61,45 +61,126 @@ def _(mo):
     return
 
 
-app._unparsable_cell(
-    r"""
-    Matplotlib version
-    """,
-    name="_"
-)
+@app.cell
+def _(mo):
+    mo.md(r"""
+    Matplotlib version of 1d PDFs. First the density
+    """)
+    return
 
 
 @app.cell
 def _(cube, dmean_e, dmean_v, drms_v, np, plt, sns):
     bins = np.geomspace(1.0, 1.0e5, 100)
-    fig, ax = plt.subplots()
+    _fig, _ax = plt.subplots()
     _H, _ = np.histogram(
         cube.di_m,
         weights=cube.e_m,
         bins=bins,
         density=True,
     )
-    ax.stairs(_H, bins, fill=True)
-
-    ax.axvline(dmean_v)
-    ax.axvline(drms_v)
-    ax.axvline(dmean_e)
-
-
-    ax.set_xscale("log")
-    ax.set_yscale("log")
-    ax.set_xlabel(
+    _ax.stairs(
+        _H * bins[:-1], bins, fill=True
+    )
+    _
+    _ax.axvline(dmean_v, color="k")
+    _ax.axvline(drms_v, color="k")
+    _ax.axvline(dmean_e, color="k")
+    _ax.set_xscale("log")
+    _ax.set_yscale("log")
+    _ax.set_xlabel(
         r"Ionized density, cm$^{-3}$"
     )
-    ax.set_ylabel(r"Probability density")
+    _ax.set_ylabel(r"Probability density")
     sns.despine()
-    fig
+    _fig
     return
 
 
 @app.cell
 def _():
     # sns.histplot(data={"x": cube.di_m, "w": cube.e_m}, x="x", weights="w", log_scale=True)
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    Then the ionization parameter
+    """)
+    return
+
+
+@app.cell
+def _(cube, np, plt, sns):
+    Ubins = np.geomspace(1e-4, 1.0e8, 100)
+    _fig, _ax = plt.subplots()
+    _H, _ = np.histogram(
+        cube.U_m,
+        weights=cube.e_m,
+        bins=Ubins,
+        density=True,
+    )
+    _ax.stairs(
+        _H * Ubins[:-1], Ubins, fill=True
+    )
+    _ax.set_xscale("log")
+    _ax.set_yscale("log")
+    _ax.set_xlabel(r"Ionization parameter")
+    _ax.set_ylabel(r"Probability density")
+    sns.despine()
+    _fig
+    return
+
+
+@app.cell
+def _(Cube, cube, dmean_e, np, plt):
+    def plot_hist2d(
+        c: Cube,
+        drange: tuple[float, float] = [
+            0.0,
+            5.0,
+        ],
+        Urange: tuple[float, float] = [
+            -4.0,
+            8.0,
+        ],
+        bins: int = 50,
+        gamma: float = 2.0,
+    ):
+        HH, _, _ = np.histogram2d(
+            x=np.log10(c.di_m),
+            y=np.log10(c.U_m),
+            bins=bins,
+            range=[drange, Urange],
+            density=True,
+            weights=c.e_m,
+        )
+        fig = plt.figure()
+        ax = fig.add_subplot()
+        # fig, ax = plt.subplots() # This is worse since type of ax cannot be inferred
+        ax.imshow(
+            HH.T ** (1 / gamma),
+            origin="lower",
+            aspect="auto",
+            extent=[*drange, *Urange],
+            cmap="magma_r",
+        )
+        # ax.contour(HH.T, extent=[*drange, *Urange])
+        ax.axvline(
+            np.log10(dmean_e),
+            c="c",
+            ls="dotted",
+        )
+        ax.axhline(0.0, c="c", ls="dotted")
+        ax.set_xlabel(r"log$_{10}$ density")
+        ax.set_ylabel(
+            r"log$_{10}$ ionization parameter"
+        )
+        return fig
+
+
+    plot_hist2d(cube, gamma=3)
     return
 
 
@@ -163,7 +244,7 @@ def _(Path):
 @app.cell
 def _(mo):
     mo.md(r"""
-    Functions and classes for reading the physical variables fpr the simulation cube
+    Functions and classes for reading the physical variables for the simulation cube
     """)
     return
 
@@ -201,6 +282,9 @@ def _(DATAPATH, fits, np):
             self.vfrac = (
                 np.sum(mask) / mask.size
             )
+            # Ionization parameter
+            self.U = self.xi**2 / self.xn
+            self.U_m = self.U[mask]
     return (Cube,)
 
 
